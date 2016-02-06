b79c29026c3399a4182321cd9c95275502736185
kazuto-seito <seito.plusr@gmail.com>
2016-02-06 23:31:48 +0900

    modify api

--- app/controllers/main_pages_controller.rb
+++ app/controllers/main_pages_controller.rb
@@ -1,3 +1,5 @@
+require 'json'
+
 class MainPagesController < ApplicationController
   protect_from_forgery except: :calc_count
 
@@ -11,12 +13,34 @@ class MainPagesController < ApplicationController
   end
   
   def calc_count
-    @counter = Counter.find_by_id(1)
-    render_error(404, "resource not found") and return unless @counter
-    @counter.count = 6
-    @counter.save!
+    
+    json = JSON.parse(request.body.read)
+    priority = json['event_data']['priority']
+    case priority
+    when 1 then
+      count = 12
+    when 2 then
+      count = 4
+    when 3 then
+      count = 2
+    else
+      count = 1
+    end
+    
+    @counter = Counter.find_by_id(FIXED_ID)
+    if @counter.nil? then
+      @counter = Counter.create do |c|
+        c.id = FIXED_ID
+        c.count = count
+      end
+    else
+      @counter.count = @counter.count + count
+      @counter.save!
+    end
+    
     respond_to do |format|
       format.html { render json: @counter}
     end
+    
   end
 end

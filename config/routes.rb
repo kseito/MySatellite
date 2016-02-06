e805c46500da30d4934d8523f46890486e2d82fe
kazuto-seito <seito.plusr@gmail.com>
2016-02-06 19:17:56 +0900

    add api

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
   get 'main_pages/help'
 
   get 'main_pages/about'
+  
+  resources :counters
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
